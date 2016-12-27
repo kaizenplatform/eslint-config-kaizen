eac0eefcdfbdd3837aedeb7787c93d8566f1849b
Yoshihide Jimbo <yjimbo@gmail.com>
2016-12-27 14:37:24 +0900

    Update rules

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ module.exports = {
     'global-require': 0,
     'arrow-body-style': 0,
     'max-len': 0,
+    'no-plusplus': [1, { 'allowForLoopAfterthoughts': true }],
     'react/prefer-stateless-function': 0,
+    'react/jsx-filename-extension': [1, { "extensions": [".js", ".jsx"] }],
+    'react/jsx-sort-props': 0,
   },
 };
